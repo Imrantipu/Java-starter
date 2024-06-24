242e14ad315818d16c40e6ee6a9bfaae90fdb719
Imran <imrantipu90@yahoo.com>
2024-06-24 21:02:07 +0200

    practiced switch statement

--- Java programming/src/Main.java
+++ Java programming/src/Main.java
@@ -11,12 +11,38 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+
+//      normal way to write switch statement
+        switch (num) {
+            case 1:
+                System.out.println("Number is 1");
+                break;
+            case 2:
+                System.out.println("Number is 2");
+                break;
+            default:
+                System.out.println("Number is neither 1 nor 2");
+                break;
+        }
 //  alternative way to write switch statement
         int num = 0;
         switch (num){
-            case 1 -> system.out.println("Number is 1");
+//            multiple lines of code can be written inside the block
+            case 1 -> {
+                system.out.println("Number is 1");
+                system.out.println("Number is 1");
+            }
             case 2 -> system.out.println("Number is 2");
             default -> system.out.println("Number is neither 1 nor 2");
         }
     }
 }
+// switch expressions for alarm 6 am for weekends and 7 am for weekdays, 8am for wendays
+  String day = "wed";
+  int time = o;
+  time = switch (day) {
+      case "sat", "sun" -> 6;
+      case "mon", "tue", "wed", "thu", "fri" -> 7;
+      default -> 8;
+//      semicolon at the end because it is switch expression
+        };
