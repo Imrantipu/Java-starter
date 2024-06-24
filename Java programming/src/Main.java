53a890e41b92f7b8ccdacff8efe09dfdfceebf73
Imran <imrantipu90@yahoo.com>
2024-06-24 20:31:23 +0200

    first commit

---
+++ Java programming/src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+//  alternative way to write switch statement
+        int num = 0;
+        switch (num){
+            case 1 -> system.out.println("Number is 1");
+            case 2 -> system.out.println("Number is 2");
+            default -> system.out.println("Number is neither 1 nor 2");
+        }
+    }
+}
